ccbac899bf3e0fece9299af91a97ba1dfb3bf8b0
Steven Nguyen <53480750+steventn@users.noreply.github.com>
2025-08-28 22:18:47 -0700

    updating references to hardcoded user credentials

--- ui/tests/login.spec.ts
+++ ui/tests/login.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { USERS } from '../fixtures/TestData';
 
 test.describe('Login Tests', () => {
   test('Valid user can log in', async ({ page }) => {
+    const { username, password } = USERS.validUser;
     const loginPage = new LoginPage(page);
     await loginPage.goto();
-    await loginPage.login('standard_user', 'secret_sauce');
+    await loginPage.login(username, password);
     await expect(page.locator('.header_label > .app_logo')).toHaveText('Swag Labs');
   });
 
   test('Invalid login shows error', async ({ page }) => {
+    const { username, password } = USERS.invalidUser;
     const loginPage = new LoginPage(page);
     await loginPage.goto();
-    await loginPage.login('wrongUser', 'badPass');
+    await loginPage.login(username, password);
     await expect(page.locator('[data-test="error"]')).toBeVisible();
     await expect(page.locator('[data-test="error"]')).toHaveText('Epic sadface: Username and password do not match any user in this service')
   });
