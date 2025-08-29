afe9ecce528e1cf45a4ab8377b314d6405c6f6d1
Steven Nguyen <53480750+steventn@users.noreply.github.com>
2025-08-28 22:15:44 -0700

    adding various user accounts to test

---
+++ ui/fixtures/TestData.ts
@@ -0,0 +1,31 @@
+export const USERS = {
+  validUser: {
+    username: 'standard_user',
+    password: 'secret_sauce',
+  },
+  lockedOutUser: {
+    username: 'locked_out_user',
+    password: 'secret_sauce',
+  },
+  problemUser: {
+    username: 'problem_user',
+    password: 'secret_sauce',
+  },
+  performanceGlitchUser: {
+    username: 'performance_glitch_user',
+    password: 'secret_sauce',
+  },
+  invalidUser: {
+    username: 'invalid_user',
+    password: 'secret_sauce',
+  },
+  visual_user: {
+    username: 'visual_user',
+    password: 'secret_sauce',
+  },
+}
+
+export const ADDRESSES = {
+  home: '123 Main St, Springfield',
+  office: '456 Elm St, Metropolis',
+};
